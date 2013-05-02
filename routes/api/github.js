f46f347db1a8e381790d675264fa7bc5f8017593
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2013-05-01 21:29:40 -0400

    base model endpoint for github/user/repo?type=statusboard

--- routes/api/github.js
+++ routes/api/github.js
@@ -1,5 +1,75 @@
+var GithubStats = require('../../lib/github');
+
+var PresModels = {
+	statusboard: require('../../lib/presModels/statusboard')
+}
+
+var countCommitsByUserInRepo = function(req, res){
+	var stats = new GithubStats(req.params.user_id, req.params.repo);
+	stats.commits(function(err, data){
+		//GITHUB ERROR
+		if(err){
+			githubError(res, err);
+		}
+		else{
+			toPresModel(req, res, data, "commitCount");
+		}
+	});
+}
+
+var githubError = function(res, err){
+	var data = {
+		msg: "GITHUB ERROR",
+		err: err
+	}
+	res.json(500, data);
+}
+
+var toPresModel = function(req, res, data, collection){
+	//TRANSFORM THIS INTO THE WISHED FOR PRES MODEL
+	
+	var type = req.query.type;
+
+	var presModel = PresModels[type];
+
+	if(presModel==undefined){
+		var data = {
+			msg: "TYPE "+type+" IS NOT VALID",
+			err: undefined
+		}
+		res.json(500, data);
+	}
+	else{
+		//transform data into the right collection of this pres model
+		presModel[collection](data, function(err, result){
+			
+			var output = {
+				graph: {
+					title: "graph title",
+					datasequences: [
+						{
+							title: "sequence title",
+							datapoints:[
+
+							]
+						}
+					]
+				}
+			}
+
+			var users = Object.keys(result);
+			for(var i=0; i<users.length; i++){
+				output.graph.datasequences[0].datapoints[i] = {title:users[i], value:result[users[i]].numCommits};
+			}
+
+			res.json(output);
+
+		});
+	}
+}
+
+
+//ROUTES
 module.exports = function(app){
-	app.get('/github/:user_id/repo', function(req, res){
-  	res.json({test:"test"});
-  });
+	app.get('/github/:user_id/:repo', countCommitsByUserInRepo);
 }
