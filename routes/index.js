50b7418b10e66519bea6e2e9fba4d8e951f9c616
Ross Kukulinski <ross.kukulinski@asti-usa.com>
2013-05-17 07:56:24 -0700

    Update main page with repo URL

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
   app.get('/', function(req, res) {
       res.render('index', {
-          title: "Node.js starter kit"
+          title: "glowing-tyrion"
       });
   });
-}
+};
